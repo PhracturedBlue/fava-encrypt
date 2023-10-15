0d2884601f6a5ad2d9be325456a6182778372afc
PhracturedBlue <rc2012@pblue.org>
2023-10-15 13:19:23 -0700

    Use inspection to find ledgers since fava no longer provides any hook to find them

--- plugins/enable_encryption.py
+++ plugins/enable_encryption.py
@@ -3,29 +3,53 @@ from typing import Iterable
 from typing import List
 import beancount.ops.documents
 import logging
+import inspect
 from beancount.core.account import is_valid
+
 __plugins__ = ['enable_encryption']
 
+# Fava does not provide any way for an extension to get access to the FavaLedger object
+# so we need to use inspection to find it.  This is incredibly brittle and terrible practice
+
 def enable_encryption(entries, options_map, dirs):
-    import fava.application
-    orig_update_ledger_slugs = fava.application.update_ledger_slugs
-    def update_ledger_slugs(ledgers):
-        for ledger in ledgers:
-            ledger._watcher = Watcher(ledger)
-        return orig_update_ledger_slugs(ledgers)
-    if orig_update_ledger_slugs != update_ledger_slugs:
-        fava.application.update_ledger_slugs = update_ledger_slugs
+    import  fava.core.watcher
+    import  fava.core
+    if fava.core.Watcher == fava.core.watcher.Watcher:
+        # This will catch any future watchers
+        fava.core.Watcher = Watcher
+        # but if we got here, a watcher has already been set that we need to fix
+        parent = inspect.currentframe()
+        while True:
+            try:
+                parent = parent.f_back
+            except:
+                break
+            try:
+                if parent.f_locals['self'].__class__ == fava.core.FavaLedger:
+                    parent.f_locals['self']._watcher = Watcher(parent.f_locals['self'])
+                    break
+            except:
+                pass
     return entries, []
 
 class Watcher:
     __slots__ = ["_encryption_file", "_watcher", "_decrypted"]
 
-    def __init__(self, ledger) -> None:
+    def __init__(self, ledger=None) -> None:
+        if ledger is None:
+            try:
+                ledger = inspect.currentframe().f_back.f_locals['self']
+                assert ledger.__class__ == fava.core.FavaLedger
+            except Exception as _e:
+                lgging.error("Failed to setup encrypted watcher: %s", _e)
         path = ledger.beancount_file_path
         self._decrypted = False
         self._encryption_file = os.path.join(os.path.dirname(path), ".encrypted")
         self.is_decrypted()
-        self._watcher = ledger._watcher
+        if hasattr(ledger, '_watcher'):
+            self._watcher = ledger._watcher
+        else:
+            self._watcher = fava.core.watcher.Watcher()
         logging.warning(f"Enable socket watcher for: {path} Decrypted: {self._decrypted}")
         pass
 
